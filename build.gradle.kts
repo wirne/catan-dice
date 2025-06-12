18312fd00e85ba7f62d0cf3a7405ff8251cbb0a1
wirne <lukas.wirne@gmail.com>
2025-06-12 06:38:20 +0200

    Bump dependencies + add privacy policy in README.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 
 
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("com.android.library") version "8.6.1" apply false
+    id("com.android.application") version "8.10.1" apply false
+    id("com.android.library") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
     id("com.google.protobuf") version "0.9.1" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21"
+    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
 }
