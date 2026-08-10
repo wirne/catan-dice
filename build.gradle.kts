0440853592f19c5026604d3b1bf8cac29ba243fd
Lukas-Wirne <Lukas.Wirne@sj.se>
2026-08-10 09:49:39 +0200

    Bump to 37

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 
 plugins {
-    id("com.android.application") version "8.9.0" apply false
-    id("com.android.library") version "8.9.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.google.protobuf") version "0.9.4" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
-    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
+    id("com.android.application") version "9.3.1" apply false
+    id("com.android.library") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("com.google.dagger.hilt.android") version "2.60.1" apply false
+    id("com.google.protobuf") version "0.10.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10"
+    id("com.google.devtools.ksp") version "2.3.6" apply false
 }
